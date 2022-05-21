6507ccf6d6cafacde7ca8110ab31c92b6d628cba
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-21 20:40:23 -0300

    Criando classe do robo

---
+++ src/main/java/gof/strategy/Robo.java
@@ -0,0 +1,14 @@
+package gof.strategy;
+
+public class Robo {
+
+    private Behavior behavior;
+
+    public void setBehavior(Behavior behavior) {
+        this.behavior = behavior;
+    }
+
+    public void toMove(){
+        behavior.toMove();
+    }
+}
