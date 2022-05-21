e658cabdae918a2315469a1bb805a13cd9f816a4
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-21 20:41:04 -0300

    Testando Desing Pattern Strategy

--- src/main/java/gof/Main.java
+++ src/main/java/gof/Main.java
@@ -3,6 +3,7 @@ package gof;
 import gof.singleton.SingletonEager;
 import gof.singleton.SingletonLazy;
 import gof.singleton.SingletonLazyHolder;
+import gof.strategy.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,5 +23,23 @@ public class Main {
         System.out.println(lazyHolder1);
         SingletonLazyHolder lazyHolder2 = SingletonLazyHolder.getInstance();
         System.out.println(lazyHolder2);
+
+        //Strategy
+
+        Behavior common = new CommonBehavior();
+        Behavior defensive = new DefensiveBehavior();
+        Behavior aggressive = new AggressiveBehavior();
+
+        Robo robo = new Robo();
+        robo.setBehavior(common);
+        robo.toMove();
+        robo.toMove();
+        robo.setBehavior(aggressive);
+        robo.toMove();
+        robo.toMove();
+        robo.toMove();
+        robo.setBehavior(defensive);
+        robo.toMove();
+
     }
 }
