a60003c29f20adbcfa93a40c5231255e84d06d9d
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-21 20:36:26 -0300

    Criando classe de comportamento defensivo

---
+++ src/main/java/gof/strategy/DefensiveBehavior.java
@@ -0,0 +1,9 @@
+package gof.strategy;
+
+public class DefensiveBehavior implements Behavior {
+
+    @Override
+    public void toMove() {
+        System.out.println("moving defensively");
+    }
+}
