dc8275d09118259870d1f549125a3fde13b7cd1f
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-21 18:15:03 -0300

    Testando as implementações do Desing Pattern de Singleton

--- src/main/java/gof/Main.java
+++ src/main/java/gof/Main.java
@@ -2,6 +2,21 @@ package gof;
 
 public class Main {
     public static void main(String[] args) {
+        //Teste Realacionado ao Desing Pattern Singleton
+        SingletonLazy lazy1 = SingletonLazy.getInstance();
+        System.out.println(lazy1);
+        SingletonLazy lazy2 = SingletonLazy.getInstance();
+        System.out.println(lazy2);
 
+
+        SingletonEager eager1 = SingletonEager.getInstance();
+        System.out.println(eager1);
+        SingletonEager eager2 = SingletonEager.getInstance();
+        System.out.println(eager2);
+
+        SingletonLazyHolder lazyHolder1 = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder1);
+        SingletonLazyHolder lazyHolder2 = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder2);
     }
 }
